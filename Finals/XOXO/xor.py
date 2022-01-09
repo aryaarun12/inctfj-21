1d31c73888a1364ab355f1783d89171630354d9e
Arya Arun <70106690+aryaarun12@users.noreply.github.com>
2022-01-09 17:07:28 +0530

    Update xor.py

--- Finals/XOXO/xor.py
+++ Finals/XOXO/xor.py
@@ -1,28 +1,25 @@
-#to find key:
+#find key
 import codecs
-a=[];
 with open('xoxo.png', 'rb') as f:
 	h = f.read().hex()
-	k=['89','50','4E','47','0D','0A','1A','0A'];ss='';aa=[]
+	k=['89','50','4E','47','0D','0A','1A','0A'];ss='';st=''
 	for y in k:
-		q=chr(int(y,16))	
-		ss=ss+q	
-	st=''
+		q=chr(int(y,16))
+		ss=ss+q
 	for c in range(0,len(h),2):
 		s=h[c]+h[c+1]
 		st=st+chr(int(s,16))
-	l=[chr(ord(a) ^ ord(b)) for a,b in zip(st, ss)]
-	print(''.join(l))  
+	key=''.join([chr(ord(a) ^ ord(b)) for a,b in zip(st, ss)])
+	print(key)
 
-'''
-#repeating xor
-import codecs
-a=[];key=b'eAsy-x0r'
-with open('xoxo.png', 'rb') as f:
-	h =f.read()
-	for i in range(0,len(h)):
-		a.append(h[i]^key[i % len(key)])
+#to retrieve image
+
+a=[];
+key=bytes(key,'utf-8')
+h =codecs.decode(h,"hex")
+for i in range(0,len(h)):
+	a.append(h[i]^key[i % len(key)])
 with open('f.png', 'wb') as m:
 	m.write(bytes(a))
 	
-'''
+
